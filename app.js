0989d012a0cb942dd7e218746d42685f88d3cb9a
Nitin Tayal <tayalnitin008@gmail.com>
2020-09-28 22:12:17 +0530

    Level-4-salting and hashing pass formed

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log(md5("000"));
 
 app.use(express.static("public"));
 app.set('view engine', "ejs");
@@ -43,38 +43,47 @@ app.get("/register",function(req,res ){
 
 
 app.post("/register",function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 
-    newUser.save(function(err){
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
+    
 
 
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({email: username}, function(err, founduser){
+    User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err);
         } else {
-            if (founduser){
-                if(founduser.password === password){
-                    res.render("secrets");
-                }
+            if (foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    if(result === true) {
+                        res.render("secrets");
+
+                    }
+                });
+                
             }
         }
-    })
-})
+    });
+});
 
 
 
